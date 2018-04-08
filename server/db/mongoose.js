372d3c99923171cda429254ae5644e936a11dba7
Rodney Gee <Rodney.Gee@momentum.co.za>
2018-04-08 13:18:45 +0200

    Stepup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,18 @@
 var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise
-mongoose.connect ('mongodb://localhost:27017/TodoApp');
+if(process.env.PORT){
+ connectPath = "ds155473.mlab.com:55473/todoapp11111";
+ options= {
+     auth: {
+         user: 'MongoUser',
+         password: 'Pentium12*'
+     }
+ }
+} else {
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+mongoose.connect(connectPath, options);
 
 module.exports = { mongoose }
