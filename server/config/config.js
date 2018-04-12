4b6d35142c406fda6f7da62d7c3eab77bd01904f
Rodney Gee <Rodney.Gee@momentum.co.za>
2018-04-12 20:41:52 +0200

    Updated expect to v21

--- server/config/config.js
+++ server/config/config.js
@@ -13,16 +13,3 @@ if (env === 'development' || env  === 'test') {
   })
 
 }
-
-
-// if (env === 'development') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-//
-// } else if (env.trim() === 'test'){
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
-// else {
-//   process.env.MONGODB_URI = 'mongodb://MongoUser:Pentium12*@ds155473.mlab.com:55473/todoapp11111'
-// }
